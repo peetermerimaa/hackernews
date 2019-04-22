5b2d44b557996f8d02280e1f270605b3876aada9
peetermerimaa <peeter.merimaa@gmail.com>
2019-04-22 23:18:38 +0300

    commit home monday

--- src/App.js
+++ src/App.js
@@ -21,11 +21,24 @@ const list = [
 
 class App extends Component { 
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);  
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
-          {list.map(function(item) {
-          return (
+          {this.state.list.map(item => 
           <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -33,8 +46,13 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
-          </div>);
-          })}
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                  Dismiss
+                </button>
+              </span>
+          </div>
+          )}
         </div>
     ); 
   }
