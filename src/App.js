83be8be8ca91b386742f909594fba4c1b65e55d5
Peeter <peeter.merimaa@datanor.ee>
2019-04-22 12:21:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component { 
   render() {
+    var helloWorld = 'Welcome to the ddRoad tokkjjhhgfgfgfggs learn React';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+          <h2>{helloWorld}</h2>
+        </div>
+    ); 
   }
 }
 
